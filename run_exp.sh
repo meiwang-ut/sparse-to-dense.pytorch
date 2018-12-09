ab0566c2aa88810c3452aef132aa2b22aa5802cb
meiwang-ut <meiwang@utexas.edu>
2018-12-09 16:40:52 -0600

    run upproj on tacc1

--- run_exp.sh
+++ run_exp.sh
@@ -1,23 +1,39 @@
 #!/usr/bin/env bash
 
-python3 main.py -a resnet50 -d deconv2 -m rgb -s 0
+## eldar-1
+#python3 main.py -a resnet50 -d deconv2 -m rgb -s 0
+#
+#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 5 --sparsifier uar
+#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 10 --sparsifier uar
+#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 100 --sparsifier uar
+##python3 main.py -a resnet50 -d deconv3 -m rgbd -s 200 --sparsifier uar
+##python3 main.py -a resnet50 -d deconv3 -m rgbd -s 1000 --sparsifier uar
+#
+##python3 main.py -a resnet50 -d deconv3 -m rgbd -s 10 --sparsifier sim_stereo
+##python3 main.py -a resnet50 -d deconv3 -m rgbd -s 100 --sparsifier sim_stereo
+##python3 main.py -a resnet50 -d deconv3 -m rgbd -s 1000 --sparsifier sim_stereo
+#
+#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 5 --sparsifier  sim_reflector
+#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 10 --sparsifier  sim_reflector
+##python3 main.py -a resnet50 -d deconv3 -m rgbd -s 20 --sparsifier  sim_reflector
+#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 100 --sparsifier  sim_reflector
+##python3 main.py -a resnet50 -d deconv3 -m rgbd -s 1000 --sparsifier  sim_reflector
+#
+##python3 main.py -a resnet50 -d deconv3 -m rgbw -s 10 --sparsifier sim_wireless
+##python3 main.py -a resnet50 -d deconv3 -m rgbw -s 100 --sparsifier sim_wireless
+##python3 main.py -a resnet50 -d deconv3 -m rgbw -s 1000 --sparsifier sim_wireless
 
-python3 main.py -a resnet50 -d deconv3 -m rgbd -s 5 --sparsifier uar
-python3 main.py -a resnet50 -d deconv3 -m rgbd -s 10 --sparsifier uar
-python3 main.py -a resnet50 -d deconv3 -m rgbd -s 100 --sparsifier uar
-#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 200 --sparsifier uar
-#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 1000 --sparsifier uar
 
-#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 10 --sparsifier sim_stereo
-#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 100 --sparsifier sim_stereo
-#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 1000 --sparsifier sim_stereo
+# tacc cs398t
+python3 main.py -a resnet50 -d upproj -m rgb -s 0
 
-python3 main.py -a resnet50 -d deconv3 -m rgbd -s 5 --sparsifier  sim_reflector
-python3 main.py -a resnet50 -d deconv3 -m rgbd -s 10 --sparsifier  sim_reflector
-#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 20 --sparsifier  sim_reflector
-python3 main.py -a resnet50 -d deconv3 -m rgbd -s 100 --sparsifier  sim_reflector
-#python3 main.py -a resnet50 -d deconv3 -m rgbd -s 1000 --sparsifier  sim_reflector
+python3 main.py -a resnet50 -d upproj -m rgbd -s 5 --sparsifier uar
+python3 main.py -a resnet50 -d upproj -m rgbd -s 10 --sparsifier uar
+python3 main.py -a resnet50 -d upproj -m rgbd -s 100 --sparsifier uar
 
-#python3 main.py -a resnet50 -d deconv3 -m rgbw -s 10 --sparsifier sim_wireless
-#python3 main.py -a resnet50 -d deconv3 -m rgbw -s 100 --sparsifier sim_wireless
-#python3 main.py -a resnet50 -d deconv3 -m rgbw -s 1000 --sparsifier sim_wireless
+python3 main.py -a resnet50 -d upproj -m rgbd -s 5 --sparsifier  sim_reflector
+python3 main.py -a resnet50 -d upproj -m rgbd -s 10 --sparsifier  sim_reflector
+python3 main.py -a resnet50 -d upproj -m rgbd -s 100 --sparsifier  sim_reflector
+
+
+# tacc tracking
